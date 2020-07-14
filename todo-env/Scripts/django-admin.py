352426b96b0a4865ba005f7b0db639b4bde6af8e
ricardoBento <ricardo@flowhospitalitytraining.co.uk>
2020-07-14 18:41:03 +0100

    c

---
+++ todo-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rbento\documents\github\drf-tdd-example\todo-env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
